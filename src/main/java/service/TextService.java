ea2a1aa7c6468246026aec322dfcf58a51506f4e
Uliana Stefanishyna <ulanka1996@gmail.com>
2019-03-23 06:30:58 +0000

    added skeletom of the exercises

---
+++ src/main/java/service/TextService.java
@@ -0,0 +1,17 @@
+package service;
+
+public class TextService {
+    public String staticText() {
+        return "Some static text";
+    }
+
+    public String variable(String variable) {
+        return variable;
+    }
+
+    public String exception(String text) throws RuntimeException {
+        //TODO throw your custom exception
+        return text;
+    }
+
+}
